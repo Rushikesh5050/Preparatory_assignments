1a34bfd50fb094d89de084bae4f8b99434a2e932
RushikeshDamugade <rushikesh9713@gmail.com>
2026-02-25 18:56:00 +0530

    added program 3

---
+++ program_3.c
@@ -0,0 +1,21 @@
+//Q3. Write a program to calculate Fibonacci Series up to n numbers
+
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, next;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series:\n");
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
